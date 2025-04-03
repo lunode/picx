3eb749b18b00f6b5032ca1ce51a0b37696725cce
lunode <48537416+lunode@users.noreply.github.com>
2025-04-03 20:23:16 +0800

    去除picx关联信息

--- src/common/api/repo.ts
+++ src/common/api/repo.ts
@@ -67,11 +67,11 @@ export const initEmptyRepo = async (
   const README = `
 # Welcome to use PicX
 
-[PicX](https://github.com/XPoet/picx) is a simple and powerful image hosting tool. It supports image hosting services via GitHub repository.
+[PicX] is a simple and powerful image hosting tool. It supports image hosting services via GitHub repository.
 
 PicX is completely open source, and you can use it for free.
 
-If you like it, please give it a star on [GitHub](https://github.com/XPoet/picx).
+If you like it, please give it a star on [GitHub](https://github.com/lunode/picx).
         `
   const { owner, selectedRepo: repo, selectedBranch: branch } = userConfigInfo
 
